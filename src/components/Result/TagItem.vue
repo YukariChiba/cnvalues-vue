c8172326bf100e0f9a4efc4adc20af72a630e972
Charles <Charles@0x7f.cc>
2020-11-06 18:08:11 +0800

    Fix Tag

--- src/components/Result/TagItem.vue
+++ src/components/Result/TagItem.vue
@@ -1,10 +1,16 @@
 <template>
   <v-list-item>
     <v-list-item-avatar class="elevation-4">
-      <v-img :src="require('@/assets/factions/' + item.id + '.png')"></v-img>
+      <v-img
+        :style="{ opacity: 'notFully' in item ? '0.5' : '' }"
+        :src="require('@/assets/factions/' + item.id + '.png')"
+      ></v-img>
     </v-list-item-avatar>
     <v-list-item-content>
-      <v-list-item-title v-html="item.name"></v-list-item-title>
+      <v-list-item-title
+        :class="'notFully' in item ? 'grey--text' : ''"
+        v-html="item.name"
+      ></v-list-item-title>
       <v-list-item-subtitle v-html="item.desc"></v-list-item-subtitle>
     </v-list-item-content>
   </v-list-item>
