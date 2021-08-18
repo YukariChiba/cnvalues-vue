fb4c505c9a7fc1b7a7fcfa8a4c91c99cd1f5d8c7
Charles Yang <Charles@0x7f.cc>
2021-08-19 00:01:07 +0800

    Add popup and dark theme fix

--- src/components/Result/TagItem.vue
+++ src/components/Result/TagItem.vue
@@ -1,25 +1,66 @@
 <template>
-  <v-list-item>
-    <v-list-item-avatar class="elevation-4">
-      <v-img
-        :style="{ opacity: 'notFully' in item ? '0.5' : '' }"
-        :src="require('@/assets/factions/' + item.id + '.png')"
-      ></v-img>
-    </v-list-item-avatar>
-    <v-list-item-content>
-      <v-list-item-title
-        :class="'notFully' in item ? 'grey--text' : ''"
-        v-html="item.name"
-      ></v-list-item-title>
-      <v-list-item-subtitle v-html="item.desc"></v-list-item-subtitle>
-    </v-list-item-content>
-  </v-list-item>
+  <v-dialog v-model="dialog" max-width="500">
+    <template v-slot:activator="{ on, attrs }">
+      <v-list-item v-bind="attrs" v-on="on">
+        <v-list-item-avatar class="elevation-4">
+          <v-img
+            :style="{ opacity: 'notFully' in item ? '0.5' : '' }"
+            :src="require('@/assets/factions/' + item.id + '.png')"
+          ></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title
+            :class="'notFully' in item ? 'grey--text' : ''"
+            v-html="item.name"
+          ></v-list-item-title>
+          <v-list-item-subtitle v-html="item.desc"></v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </template>
+    <v-card>
+      <v-card-title class="justify-center avatar-section py-0">
+        <v-avatar size="128" color="primary" class="avatar">
+          <v-img
+            :style="{ opacity: 'notFully' in item ? '0.5' : '' }"
+            :src="require('@/assets/factions/' + item.id + '.png')"
+          ></v-img>
+        </v-avatar>
+      </v-card-title>
+      <v-card-title class="text-h5">
+        {{ item.name }}
+      </v-card-title>
+      <v-card-text>{{ item.desc }}</v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
-  props: ["item"]
+  props: ["item"],
+  data: () => ({
+    dialog: false
+  })
 };
 </script>
 
-<style></style>
+<style scoped>
+.v-card {
+  overflow-y: visible !important;
+}
+
+.avatar {
+  position: relative;
+  margin-bottom: -64px;
+  top: -64px;
+}
+
+.avatar-section {
+  overflow: visible;
+}
+</style>
+
+<style>
+.v-dialog {
+  overflow-y: visible !important;
+}
+</style>
--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-footer dark padless class="primary lighten-1 white--text text-center">
+  <v-footer
+    dark
+    padless
+    class="primary white--text text-center"
+    :class="$vuetify.theme.dark ? 'darken-1' : 'lighten-1'"
+  >
     <v-card-text>
       <div>本测试受以下测试启发完成：</div>
       <v-btn text v-for="link in links" :key="link.name">{{ link.name }}</v-btn>
